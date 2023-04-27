ade62c796c465518da05dc4f6984814541a6cfe0
sssinty <vzin@list.ru>
2023-04-27 15:16:17 +0300

    First task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,14 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = Math.pow(b, 2) - (4 * a * c);
+  if (discriminant > 0) {
+    arr.push((-b + Math.sqrt(discriminant) )/(2 * a), (-b - Math.sqrt(discriminant) ) / (2 * a));
+  }else if (discriminant == 0) {
+    arr.push(-b / (2 * a));
+  }else if (discriminant < 0) {
+    return arr;
+  }
   return arr;
 }
 
