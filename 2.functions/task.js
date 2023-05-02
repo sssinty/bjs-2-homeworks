f8484be29c040d682e672e56d59427f356cf19af
sssinty <vzin@list.ru>
2023-05-02 23:48:23 +0300

    (Fu)Решение первого задания.

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,15 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+  let max = Math.max.apply(null, arr);
+  let min = Math.min.apply(null, arr);
+  let sum = 0;
+  let avg = 0;
+  for(let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  avg = sum / arr.length;
+  return { min: min, max: max, avg: +avg.toFixed(2) };
 }
+console.log( getArrayParams(1, 2, 3, -100, 10))
 
 function summElementsWorker(...arr) {
 
