e9a45661c1c52b181ca2621c2af907855e062f1c
sssinty <vzin@list.ru>
2023-05-15 11:41:42 +0300

    Решение 4 задания (объекты)

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,50 @@
+
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
   
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if(marks != undefined) {
+    this.marks = marks
+    // marks.push(marks);
+  }
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function (...marks) {
+  if(student1.marks === undefined || student1.marks.length === 0) {
+    return 0;
+  }else{
+    let sum = student1.marks.reduce((acc, number) => acc + number, 0);
+    let arrLength = student1.marks.length;
+    return sum / arrLength;
+  }
+    
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.reason = reason;
+  delete this.marks
+  delete this.subject
+
 }
+
+// let student1 = new Student("Василиса", "женский", 19);
+// student1.setSubject("Algebra");
+// console.log(student1.getAverage()); // 0
+// student1.addMarks(4, 5, 4, 5);
+// console.log(student1.getAverage()); // 4.5
+// console.log(student1);
+
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба')
+console.log(student2)
+
