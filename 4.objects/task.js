9b45e38a6d337fd4d8ed31e52d21afbae02112ed
sssinty <vzin@list.ru>
2023-05-15 21:18:27 +0300

    Исправление ошибок

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -9,18 +9,18 @@ function Student(name, gender, age) {
 
 Student.prototype.setSubject = function (subjectName) {
   if(this.reason === undefined) {
-    this.subject = subjectName
+    this.subject = subjectName;
   }
 }
 
 Student.prototype.addMarks = function (...marks) {
-    if(this.marks != undefined && this.reason === undefined) {
-    this.marks.push(...marks)
+    if(this.marks != undefined && this.exclude != undefined) {
+    this.marks.push(...marks);
   }
 }
 
 Student.prototype.getAverage = function (...marks) {
-  if(this.marks === undefined || this.marks.length === 0 || this.reason != undefined) {
+  if(this.marks === undefined || this.marks.length === 0 || this.exclude === undefined) {
     return 0;
   }else {
     let sum = this.marks.reduce((acc, number) => acc + number, 0);
@@ -31,27 +31,9 @@ Student.prototype.getAverage = function (...marks) {
 }
 
 Student.prototype.exclude = function (reason) {
-  this.reason = reason;
-  if(this.reason != undefined) {
-    delete this.marks
-    delete this.subject
+  this.exclude = reason;
+  if(this.exclude != undefined) {
+    delete this.marks;
+    delete this.subject;
   }
 }
-
-// let student = new Student("Василиса", "женский", 19);
-// student.setSubject("Algebra");
-// console.log(student.getAverage()); // 0
-// student.addMarks(5, 4, 5);
-// student.addMarks(5, 4, 5);
-// console.log(student.getAverage()); // 4.5
-// console.log(student);
-// console.log(student.marks);
-
-// let student = new Student("Артём", "мужской", 25);
-// student.exclude('прогулы');
-// student.setSubject("Geometry");
-// student.addMarks(4, 5, 4, 5);
-// console.log(student.getAverage())
-// console.log(student.subject);
-// console.log(student.marks);
-// console.log(student);
