bf8c47f1d41c75397c794dd54819f043721701f6
sssinty <vzin@list.ru>
2023-05-15 23:05:28 +0300

    Исправиление ошибок в тесте 5, 6

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -8,13 +8,13 @@ function Student(name, gender, age) {
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  if(this.reason === undefined) {
+  if(this.exclude === undefined) {
     this.subject = subjectName;
   }
 }
 
 Student.prototype.addMarks = function (...marks) {
-    if(this.marks != undefined && this.exclude != undefined) {
+    if(this.marks != undefined || this.exclude === undefined) {
     this.marks.push(...marks);
   }
 }
@@ -37,3 +37,22 @@ Student.prototype.exclude = function (reason) {
     delete this.subject;
   }
 }
+// let student = new Student("Василиса", "женский", 19);
+// student.exclude('прогулы');
+// student.setSubject("Algebra");
+// console.log(student.getAverage()); // 0
+// student.addMarks(5, 4, 5);
+// console.log(student.getAverage()); // 4.5
+
+// console.log(student);
+// console.log(student.marks);
+
+// let student = new Student("Артём", "мужской", 25);
+// student.addMarks(4, 5, 4, 5);
+// student.exclude('прогулы');
+// student.addMarks(4, 5, 4, 5);
+// console.log(student.getAverage())
+// student.addMarks(4, 5, 4, 5);
+// console.log(student.subject);
+// console.log(student.marks);
+// console.log(student);
