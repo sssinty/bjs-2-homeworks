55f27b4ac5b2d8fa1a7c9d084a31089d468df40d
sssinty <vzin@list.ru>
2023-05-15 17:26:50 +0300

    Исправление ошибок в 3-х тестах

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -15,12 +15,12 @@ Student.prototype.setSubject = function (subjectName) {
 
 Student.prototype.addMarks = function (...marks) {
     if(this.marks != undefined && this.reason === undefined) {
-    this.marks = marks
+    this.marks.push(...marks)
   }
 }
 
 Student.prototype.getAverage = function (...marks) {
-  if(this.marks === undefined || this.marks.length === 0) {
+  if(this.marks === undefined || this.marks.length === 0 || this.reason != undefined) {
     return 0;
   }else {
     let sum = this.marks.reduce((acc, number) => acc + number, 0);
@@ -41,12 +41,17 @@ Student.prototype.exclude = function (reason) {
 // let student = new Student("Василиса", "женский", 19);
 // student.setSubject("Algebra");
 // console.log(student.getAverage()); // 0
-// student.addMarks(4, 5, 4, 5);
+// student.addMarks(5, 4, 5);
+// student.addMarks(5, 4, 5);
 // console.log(student.getAverage()); // 4.5
 // console.log(student);
+// console.log(student.marks);
 
 // let student = new Student("Артём", "мужской", 25);
-// student.exclude('плохая учёба');
+// student.exclude('прогулы');
 // student.setSubject("Geometry");
 // student.addMarks(4, 5, 4, 5);
+// console.log(student.getAverage())
+// console.log(student.subject);
+// console.log(student.marks);
 // console.log(student);
