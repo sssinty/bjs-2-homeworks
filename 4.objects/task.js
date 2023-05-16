74f8ab1930a7818ee98091fadb66aec7a410a5f2
sssinty <vzin@list.ru>
2023-05-16 11:23:45 +0300

    Исправление ошибок

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -8,19 +8,19 @@ function Student(name, gender, age) {
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  if(this.exclude === undefined) {
-    this.subject = subjectName;
+  if(student.hasOwnProperty('exclude') === false) {
+    this.subject = subjectName; 
   }
 }
 
 Student.prototype.addMarks = function (...marks) {
-    if(this.marks != undefined || this.exclude === undefined) {
+    if(this.marks != undefined || student.hasOwnProperty('exclude') === false) {
     this.marks.push(...marks);
   }
 }
 
 Student.prototype.getAverage = function (...marks) {
-  if(this.marks === undefined || this.marks.length === 0 || this.exclude === undefined) {
+  if(this.marks === undefined || this.marks.length === 0 || student.hasOwnProperty('exclude') === true) {
     return 0;
   }else {
     let sum = this.marks.reduce((acc, number) => acc + number, 0);
@@ -32,27 +32,26 @@ Student.prototype.getAverage = function (...marks) {
 
 Student.prototype.exclude = function (reason) {
   this.exclude = reason;
-  if(this.exclude != undefined) {
+  if(student.hasOwnProperty('exclude') === true) {
     delete this.marks;
     delete this.subject;
   }
 }
+
 // let student = new Student("Василиса", "женский", 19);
-// student.exclude('прогулы');
 // student.setSubject("Algebra");
 // console.log(student.getAverage()); // 0
-// student.addMarks(5, 4, 5);
+// student.addMarks(4, 5, 4, 5);
 // console.log(student.getAverage()); // 4.5
-
+// console.log(student);
 // console.log(student);
 // console.log(student.marks);
 
 // let student = new Student("Артём", "мужской", 25);
+// student.exclude('плохая учёба')
 // student.addMarks(4, 5, 4, 5);
-// student.exclude('прогулы');
-// student.addMarks(4, 5, 4, 5);
-// console.log(student.getAverage())
-// student.addMarks(4, 5, 4, 5);
-// console.log(student.subject);
+// console.log(student.getAverage()); // 0
 // console.log(student.marks);
+// student.setSubject("Geometry");
 // console.log(student);
+
