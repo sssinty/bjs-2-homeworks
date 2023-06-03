7545809f786df5f2ce5bb176d0bdaca225794ed0
sssinty <vzin@list.ru>
2023-06-03 20:55:37 +0300

    Решение задачи №7 «Асинхронность»

---
+++ 7.async/task.js
@@ -0,0 +1,54 @@
+class AlarmClock {
+	constructor (alarmCollection  = [], intervalId,) {
+			this.alarmCollection = alarmCollection;
+			this.intervalId = intervalId;
+	}
+
+	addClock(time, callback) {
+		if(!time || !callback) {
+				throw new Error('Отсутствуют обязательные аргументы');
+		}else if(this.alarmCollection.some((item) => item.time === this.time)) {
+				console.warn('Уже присутствует звонок на это же время');
+		}
+			this.alarmCollection.push({time, callback, canCall: true});
+	}
+	
+	removeClock(time) {
+		this.alarmCollection = this.alarmCollection.filter(call => call.time != time);
+	}
+
+	getCurrentFormattedTime() {
+		return new Date().toLocaleDateString('ru-RU', {hour: '2-digit', minute: '2-digit'});
+	}
+
+	start() {
+		if(this.intervalId) {
+				return;
+		}
+		this.intervalId = setInterval(() => {
+			this.alarmCollection.forEach(item => {
+				if(item.time === this.getCurrentFormattedTime() && item.canCall) {
+					item.canCall = false;
+					item.callback();
+				}
+			});
+		}, 1000);
+
+			
+	}
+	
+	stop() {
+		clearInterval(this.intervalId);
+		this.intervalId = null;
+	}
+
+	resetAllCalls() {
+		this.alarmCollection.forEach(item => {item.canCall = true});
+	}
+
+	clearAlarms() {
+		this.stop();
+		this.alarmCollection = [];
+	}
+}
+
